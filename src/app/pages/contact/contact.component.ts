d92bf35a7d143fb513ac8940d384ad3a187d9cd8
Jisna George <jisnageorge0143@gmail.com>
2024-06-25 09:58:12 +0530

    second commit

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../../components/navbar/navbar.component";
 import { FooterComponent } from "../../components/footer/footer.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'app-contact',
     standalone: true,
     templateUrl: './contact.component.html',
     styleUrl: './contact.component.scss',
-    imports: [NavbarComponent, FooterComponent]
+    imports: [NavbarComponent, FooterComponent,RouterLink]
 })
 export class ContactComponent {
 
